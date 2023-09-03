a4fcc66b2696cb0416bce74f7eb51dc9c668c181
Naoki04 <naoki.shitanda.phone@gmail.com>
2023-09-03 14:24:55 +0900

    ゲーム管理DBの仕様変更

--- DynamoDB/README.md
+++ DynamoDB/README.md
@@ -2,12 +2,13 @@
 参考: https://www.wakuwakubank.com/posts/639-aws-dynamodb-introduction/
 
 ## game_manager(ゲーム管理テーブル)
-1ゲームを1行で管理するテーブル。ゲームの進行状態やメンバー識別子などを記録する。\
-- GameID: 1から順に振っていく整数。
+1ゲームを1行で管理するテーブル。ゲームの進行状態などを記録する。
 - RoomID: ランダムな6桁の整数。
-- n_mem: そのゲームに参加する人数(3~8人)
-- OwnerID: 部屋の作成者の識別子
-- UserIDs: 参加者の識別子(リスト)
+- Password: ６桁のアルファベット
+- N_mem: そのゲームに参加する人数(3~8人)
+- Members: []
+- N_hacked: 人数(1~参加者の半数未満)
+- Hacked: []
 - State: ゲームの状態
     - 0: 募集中
     - 1: ゲーム中
@@ -15,16 +16,12 @@
     - 3: ゲーム取り消し・中断
 - CreatedAt: ゲームが作成された時刻(yyyymmdd-hh-mm-ss形式で文字列)
 
-|Partiation Key|SortKey|Attribute|Attribute|Attribute|Attribute|Attribute|Attribute|
+|Partiation Key|Attribute|Attribute|Attribute|Attribute|Attribute|Attribute|Attribute|
 |--|--|--|--|--|--|--|--|
-|GameID|RoomID|n_mem(人数)|OwnerID|MemberIDs|n_hacked|Hacked|State|created-at|
-|1|703712|5|"ajlra321"|["daow121", "daw131", "sgsrg21", "fjea412"]|1|[]|0|yyyymmdd-hh-mm-ss|
-|2|732712|4|"daow134"|["ddawow121", "daw131", "fjea412"]|1|[]|0|yyyymmdd-hh-mm-ss|
+|RoomID(int)|Password(str)|n_mem(int)|Members(str list)|N_hacked(int)|Hacked(str list)|State(int)|Created-at|
+|201301|"djakdl"|3|[""]|1|[""]|0|"20130912-12-31-01"|
+|312001|"alfkl"|5|[""]|2|[""]|0|"20130912-12-34-01"|
 
-
-## ゲーム進行テーブル
-ゲーム内での発言などを記録するテーブル。
-|質問ID|ゲームID|回答||
 
 ## 質問テーブル
 事前に用意した質問から出題するモードで使う質問を格納しておくテーブル
---
+++ DynamoDB/test.py
@@ -0,0 +1,60 @@
+import boto3
+
+table_name = 'game_manager_2'
+
+dynamodb = boto3.resource(
+        "dynamodb", 
+        region_name='ap-northeast-3',
+        aws_access_key_id='AKIA3A2XCT5QQKAEUDUF',
+        aws_secret_access_key='T6j/4M8ykfMF4/AwFovF27xhgPsKSnNF2wCRqRpz',
+        #aws_session_token='YOUR_SESSION_TOKEN
+        )
+
+def make_game_table():
+    
+    table = dynamodb.create_table(
+        TableName=table_name,
+        KeySchema=[
+            {
+                'AttributeName': 'RoomID',
+                'KeyType': 'HASH'  # Partition key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'RoomID',
+                'AttributeType': 'N'
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+
+def new_item():
+    table = dynamodb.Table(table_name)
+    table.put_item(
+        Item={
+            'RoomID': 219211,
+            'Password': "alaksa",
+            'N_mem': 4,
+            'Members': ["asae", "asda"],
+            'N_hacked': 1,
+            'Hacked': [],
+            'State': 0,
+            'Created-at': '20230931-09-12-43'
+        }
+    )
+    return  0
+
+def main():
+    # ゲーム管理テーブルの作成
+    ## テーブル作成をAWSマネジメントコンソールで行うとkeyschemaがHASHにならないので, scanで条件指定した際に怒られる。
+    #make_game_table()
+    new_item()
+    return 0
+
+
+if __name__ == '__main__':
+    main()
